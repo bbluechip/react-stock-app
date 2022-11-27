c580c7a150115f63cfe9a3b278e91d1532191d6c
bbluechip <cs.orhan.berk@gmail.com>
2022-11-27 10:33:48 +0100

    add firm card and edit

--- src/pages/Firms.jsx
+++ src/pages/Firms.jsx
@@ -1,16 +1,37 @@
+import { Box, Button, Grid, Typography } from "@mui/material";
 import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import FirmCard from "../components/FirmCard";
 import useStockCalls from "../hooks/useStockCalls";
 
 const Firms = () => {
-  const { getFirms, getSales } = useStockCalls();
+  const { getFirms } = useStockCalls();
+  const { firms } = useSelector((state) => state.stock);
 
   useEffect(() => {
     getFirms();
-    getSales();
     // eslint-disable-next-line
   }, []);
 
-  return <div>Firms</div>;
+  return (
+    <Box>
+      <Typography variant="h4" color="error" mb={4}>
+        Firms
+      </Typography>
+
+      <Button variant="contained">New Firm</Button>
+
+      {firms?.length > 0 && (
+        <Grid container justifyContent="center" gap={3}>
+          {firms?.map((firm) => (
+            <Grid item>
+              <FirmCard key={firm.id} firm={firm} />
+            </Grid>
+          ))}
+        </Grid>
+      )}
+    </Box>
+  );
 };
 
 export default Firms;
